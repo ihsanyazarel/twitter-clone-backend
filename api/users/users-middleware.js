9343b333f151cc1abe6479dcc1ed9b277e46b97e
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-07-12 16:18:22 +0300

    users-middleware finished

---
+++ api/users/users-middleware.js
@@ -0,0 +1,39 @@
+const { getUserById } = require("./users-model");
+
+const idValidation = async (req,res,next) => {
+    try {
+        const user = await getUserById(req.params.id);
+        if(user){
+            req.user = user;
+            next()
+        } else {
+            res.status(404).json({messagge: "Kullanıcı bulunamadı!"});
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+const pyldVld = (req,res,next) => {
+    try {
+        const allowedKeys = ["nickName", "userName", "userSurname", "userEmail"];
+        const sentAllowedKeys = Object.keys(req.body).filter(key => allowedKeys.includes(key));
+        if(sentAllowedKeys.length > 0){
+            const newUser = {};
+            sentAllowedKeys.forEach(item => {
+                newUser[item] = req.body[item];
+            });
+            req.newUser = newUser;
+            next()
+        } else {
+            res.status(400).json({message: "Güncellemek istediğiniz bilgileri giriniz!"});
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    idValidation,
+    pyldVld,
+}
