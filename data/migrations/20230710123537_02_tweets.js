a8f8b8d9da236002ac24fab09fc013dcffd6a8fa
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-07-13 03:08:03 +0300

    column order updated

--- data/migrations/20230710123537_02_tweets.js
+++ data/migrations/20230710123537_02_tweets.js
@@ -5,10 +5,10 @@
 exports.up = function(knex) {
     return knex.schema.createTable('Tweets', function(tweets) {
         tweets.increments("tweet_id");
+        tweets.integer("user_id").references("user_id").inTable("Users").onDelete("CASCADE").onUpdate("CASCADE")
         tweets.string('tweetContent', 1024).notNullable();
         tweets.integer("numberOfLikes").defaultTo(0)
         tweets.integer("numberOfComments").defaultTo(0)
-        tweets.integer("user_id").references("user_id").inTable("Users").onDelete("CASCADE").onUpdate("CASCADE")
         tweets.timestamps(true, true);
       });
 };
