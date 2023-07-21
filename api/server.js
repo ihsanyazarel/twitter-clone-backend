0a99fcc3f5205e322b32420311677b1b3faf154b
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-07-21 14:29:15 +0300

    updated

--- api/server.js
+++ api/server.js
@@ -19,8 +19,16 @@ server.use(morgan("dev"));
 
 // routers
 server.get("/", (req,res)=>{
-    res.send("<h1>Backend-Challenge Projesi(Twitter clone), server ayakta...</h1>")
-})
+    res.send(`
+    <div style="text-align: center;">
+        <h1>Twitter Klon Projesi'ne Hoşgeldiniz</h1>
+        <img src="/doge-clipart-3.jpg" alt="Twitter Logo" style="width: 30%;" />
+        <a style="text-decoration: none" href= "https://ihsanyazarel-portfolio.vercel.app/">
+            <p style="font-weight: bold"> İhsan YAZAREL | 2023 </p>
+        </a>
+    </div>
+`)
+}).use(express.static('documents'))
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", restricted, usersRouter);
@@ -30,8 +38,9 @@ server.use("/api/comments", restricted, commentsRouter);
 
 // error mw
 server.use((err,req,res,next)=>{
-    res.status(err.status || 500).json({message: err.message || "Server Error!"});
+    res.status(err.status || 500).json({message: err.message || "Sunucu Hatası!"});
 })
 
 // export
 module.exports = server;
+
